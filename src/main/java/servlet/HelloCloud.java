8c5736b42284898aee055ebfce92906abc190097
akawecki <akawecki@plus.pl>
2018-10-30 14:30:09 +0100

    'initial'

---
+++ src/main/java/servlet/HelloCloud.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloCloud extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + "hello from cloud" + "</h1>");
+    }
+}
